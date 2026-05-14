eb0a7e9c0e757bdca7b5b48becc0c98ae5ac1a28
TarikThV <babaniz.tarik@gmail.com>
2026-05-14 15:42:11 +0300

    Add more config options to improve adaptability and usability, reduce boilerplate code.
        -Added TextConfig class to hold the text settings for every property type.
        -Added TextSettings class to hold every text and its properties like font, font size etc.
        -Changed name of Committee class to CommitteeConfig class.
        -Added CommitteeConfig class to not only hold text colors, but all the ext properties for the text of every property type in textConfigs.
        -Added a property named textConfigPaths to CommitteeConfig class to hold all the TextConfig's paths.
        -Removed fontConfigPath from Config as we don't need it anymore.
        -Removed isPersonnel from Config as we don't need it anymore.
        -Added prepareTextConfig function to load the font to a Buffer object to use it in every text drawing.
        -Added prepareText function to prepare the TextSettings object as defined in the given TextConfig object.
        -Removed unnecesary lines from processUser function as well as isPersonnel parameter.
        -Adjusted processUser function to implement the usage of processText and prepareText functions to reduce boilerplate and improve adaptability.
        -Added loadTextConfigs function to load TextConfig objects.
        -Changed loadTemplates function to be prepareCommitteeConfigs function and added lines to load and prepare TextConfigs.
        -Removed loadFontConfig function as we don't need have any FontConfig.
        -Adjusted processAll function to adapt the new logic.

--- scripts/yakakart.ts
+++ scripts/yakakart.ts
@@ -1,28 +1,25 @@
 import fs from "fs";
 import path from "path";
 import {
+  asNumber,
+  Color,
   componentsToColor,
   PDFDocument,
+  PDFFont,
   PDFPage,
   rgb,
   StandardFonts,
 } from "pdf-lib";
 import { v4 as uuidv4 } from "uuid";
-import fontkit from "@pdf-lib/fontkit";
+import fontkit, { Font } from "@pdf-lib/fontkit";
 import * as QRCode from "qrcode";
 
-class Committee {
+class CommitteeConfig {
   constructor(
-    public template: string,
-    public textColor: number[],
+    public templatePath: string,
     public templateDoc: PDFDocument,
-  ) { }
-}
-
-class FontConfig {
-  constructor(
-    public fontPath: string,
-    public fontSize: number,
+    public textConfigPaths: string[],
+    public textConfigs: TextConfig[]
   ) { }
 }
 
@@ -42,12 +39,10 @@ class User {
 class Config {
   constructor(
     public committeeConfigPath: string,
-    public fontConfigPath: string,
     public failedUsersPath: string,
     public successfulUsersPath: string,
     public templatesPath: string,
     public usersPath: string,
-    public isPersonnel: boolean | null,
   ) { }
 }
 
@@ -57,6 +52,35 @@ class ProcessingResult {
     public successful: boolean,
     public reason: string | null,
   ) { }
+}
+
+
+class TextConfig {
+  constructor(
+    public field: string,
+    public fontPath: string,
+    public fontBytes: Buffer,
+    public x: string,
+    public y: string,
+    public fontSize: string,
+    public color: number[],
+    public prepared: boolean = false,
+    public page: string,
+  ) { }
+
+}
+class TextSettings {
+  constructor(
+    public text: string,
+    public font: PDFFont,
+    public x: string,
+    public y: string,
+    public fontSize: string,
+    public color: number[],
+    public page: string,
+  ) {
+  }
+
 }
 
 enum ProcessingMode {
@@ -78,6 +102,7 @@ const loadConfig = async (configPath: string) => {
   const config: Config = JSON.parse(fs.readFileSync(configPath, "utf8"));
   return config;
 }
+
 
 const removeSpaces = (str: string) => {
   return str
@@ -115,7 +140,44 @@ const test = async () => {
   fs.writeFileSync("./results/test.pdf", pdfBytes);
 };
 
-const processUser = async (user: User, committeeConfig: Record<string, Committee>, fontConfig: FontConfig, isPersonnel: boolean = false) => {
+
+const prepareTextConfig = async (pdfDoc: PDFDocument, textConfig: TextConfig) => {
+  if (!fs.existsSync(textConfig.fontPath)) {
+    throw new Error(`Couldn't find font in: ${textConfig.fontPath}!`);
+  }
+  textConfig.fontBytes = fs.readFileSync(textConfig.fontPath);
+  textConfig.prepared = true;
+}
+
+const prepareText = async (textConfig: TextConfig, object: any, pdfDoc: PDFDocument) => {
+  if (!textConfig.prepared) {
+    throw new Error(`TextConfig is not prepared, call prepareTextConfig first!`);
+  }
+  if (!object[textConfig.field]) {
+    throw new Error(`Object ${object.toString()} has no field named: ${textConfig.field}`);
+  }
+  if (object[textConfig.field] == null) {
+    throw new Error(`Object ${object.toString()} has a null field named: ${textConfig.field}`);
+  }
+  const text = object[textConfig.field].toString();
+  pdfDoc.registerFontkit(fontkit);
+  const font = await pdfDoc.embedFont(textConfig.fontBytes);
+  return new TextSettings(text, font, textConfig.x, textConfig.y, textConfig.fontSize, textConfig.color, textConfig.page);
+}
+
+const processText = async (text: TextSettings, pdfDocument: PDFDocument) => {
+  const pdfPage = pdfDocument.getPage(eval(text.page));
+  const width = text.font.widthOfTextAtSize(text.text, asNumber(eval(text.fontSize)));
+  const height = text.font.heightAtSize(asNumber(eval(text.fontSize)));
+  pdfPage.moveTo(eval(text.x), eval(text.y));
+  pdfPage.drawText(text.text, {
+    color: componentsToColor(text.color),
+    font: text.font,
+    size: asNumber(eval(text.fontSize)),
+  })
+}
+
+const processUser = async (user: User, committeeConfig: Record<string, CommitteeConfig>) => {
   // Fixing typo issues in names
   if (user.fullName == null) {
     throw new Error("There's no name!");
@@ -123,73 +185,24 @@ const processUser = async (user: User, committeeConfig: Record<string, Committee
   if (user.committee == null) {
     throw new Error("There's no committee!");
   }
-  if (user.nationality == null && !isPersonnel) {
-    throw new Error("There's no nationalitiy!");
-  }
-  if (user.job == null && isPersonnel) {
-    throw new Error("There's no job!");
-  }
   if (user.userId == null && user.personnelId == null) {
     throw new Error("The user has no id!");
   }
-  user.fullName = titleCase(user.fullName);
-  user.fullName = removeSpaces(user.fullName);
   // Check and load committee config
   const committee = committeeConfig[user.committee];
   if (!committee) {
     throw new Error("Invalid committee name!");
   }
-  // Check for nationality to not to be empty
-  if (user.nationality == "") {
-    throw new Error("There's no nationality!");
-  }
+  user.fullName = titleCase(user.fullName);
+  user.fullName = removeSpaces(user.fullName);
   // Copy the desired doc for the committee
   let pdfDoc = await committee.templateDoc.copy();
-  // Register fontkit and load font settings
-  pdfDoc.registerFontkit(fontkit);
-  const fontBytes = fs.readFileSync(fontConfig.fontPath);
-  let font = await pdfDoc.embedFont(fontBytes, { subset: false });
-  // Use first page to edit
-  let firstPage = pdfDoc.getPages()[0];
-  let { width, height } = firstPage.getSize();
-  // Set the text settings
-  let textWidth = font.widthOfTextAtSize(user.fullName, 18);
-  let textHeight = font.heightAtSize(18);
-  let text = user.fullName;
-  // Draw the text
-  // firstPage.moveTo(
-  //   (width - textWidth) / 2,
-  //   (height - textHeight) / 2 - textHeight * 0,
-  // );
-  firstPage.moveTo(
-    (width - textWidth) / 2,
-    160,
-  );
-  firstPage.drawText(text, {
-    size: 18,
-    font: font,
-    color: componentsToColor(committeeConfig[user.committee].textColor),
-  });
-  if (isPersonnel && user.job != null) {
-    text = user.job;
+  for (const textConfig of committee.textConfigs) {
+    const text = await prepareText(textConfig, user, pdfDoc);
+    await processText(text, pdfDoc);
   }
-  else if (user.nationality != null) {
-    text = user.nationality;
-  }
-  textWidth = font.widthOfTextAtSize(text, fontConfig.fontSize);
-  // firstPage.moveTo(
-  //   (width - textWidth) / 2,
-  //   (height - textHeight) / 2 - textHeight * 1.5,
-  // );
-  firstPage.moveTo(
-    (width - textWidth) / 2,
-    134,
-  );
-  firstPage.drawText(text, {
-    size: fontConfig.fontSize,
-    font: font,
-    color: componentsToColor(committeeConfig[user.committee].textColor),
-  });
+  const firstPage = pdfDoc.getPage(0);
+
   const id = user.userId ? user.userId.toString() : user.personnelId ? user.personnelId : "NULL";
   const qrCodeData = await QRCode.toBuffer(id, {
     width: 200,
@@ -205,11 +218,20 @@ const processUser = async (user: User, committeeConfig: Record<string, Committee
   return pdfDoc;
 }
 
+const loadTextConfigs = async (textConfigPath: string) => {
+  if (!fs.existsSync(textConfigPath)) {
+    throw new Error("Text config file not found!");
+  }
+  const textConfig: TextConfig[] = JSON.parse(fs.readFileSync(textConfigPath, "utf8"));
+  return textConfig;
+}
+
+
 const loadCommitteeConfig = async (committeeConfigPath: string) => {
   if (!fs.existsSync(committeeConfigPath)) {
     throw new Error("Committee config file not found!");
   }
-  const committeeConfig: Record<string, Committee> = JSON.parse(
+  const committeeConfig: Record<string, CommitteeConfig> = JSON.parse(
     fs.readFileSync(committeeConfigPath, "utf8")
   );
   return committeeConfig;
@@ -225,7 +247,9 @@ const loadUsers = async (usersPath: string) => {
   return users;
 }
 
-const loadTemplates = async (committeeConfig: Record<string, Committee>, templatesPath: string) => {
+
+
+const prepareCommitteeConfigs = async (committeeConfig: Record<string, CommitteeConfig>, templatesPath: string) => {
   if (!fs.existsSync(templatesPath)) {
     throw new Error("Templates folder not found!");
   }
@@ -233,24 +257,26 @@ const loadTemplates = async (committeeConfig: Record<string, Committee>, templat
     throw new Error("Committee config not loaded! Load it first!");
   }
   for (const [committeeName, committee] of Object.entries(committeeConfig)) {
-    if (!fs.existsSync(path.join(templatesPath, committee.template))) {
+    if (!fs.existsSync(path.join(templatesPath, committee.templatePath))) {
       throw new Error(`Template file for ${committeeName} not found!`);
     }
     committeeConfig[committeeName].templateDoc = await PDFDocument.load(
-      fs.readFileSync(path.join(templatesPath, committee.template)),
+      fs.readFileSync(path.join(templatesPath, committee.templatePath)),
     );
+    committeeConfig[committeeName].textConfigs = [];
+    for (const textConfigPath of committee.textConfigPaths) {
+      if (!fs.existsSync(textConfigPath)) {
+        throw new Error(`Text configs for ${committee} not found!`);
+      }
+      const textConfig = JSON.parse(
+        fs.readFileSync(textConfigPath, "utf8")
+      );
+      committeeConfig[committeeName].textConfigs.push(textConfig);
+      prepareTextConfig(committee.templateDoc, textConfig);
+    }
   }
 }
 
-const loadFontConfig = async (fontConfigPath: string) => {
-  if (!fs.existsSync(fontConfigPath)) {
-    throw new Error("Font config file not found!");
-  }
-  const fontConfig: FontConfig = JSON.parse(
-    fs.readFileSync(fontConfigPath, "utf8"),
-  );
-  return fontConfig;
-}
 
 const createResultsFolder = async () => {
   const resultsFolder = path.join("./results/", uuidv4());
@@ -275,9 +301,8 @@ const loadProcessingResults = async (processingResultsPath: string) => {
 const processAll = async (processingMode: ProcessingMode, configPath: string) => {
   const config = await loadConfig(configPath);
   const committeeConfig = await loadCommitteeConfig(config.committeeConfigPath);
-  const fontConfig = await loadFontConfig(config.fontConfigPath);
   const resultsFolder = await createResultsFolder();
-  await loadTemplates(committeeConfig, config.templatesPath);
+  await prepareCommitteeConfigs(committeeConfig, config.templatesPath);
   let users: User[] = [];
 
 
@@ -298,7 +323,7 @@ const processAll = async (processingMode: ProcessingMode, configPath: string) =>
 
   for (const user of users) {
     try {
-      let userDocument = await processUser(user, committeeConfig, fontConfig, config.isPersonnel ? config.isPersonnel : false);
+      let userDocument = await processUser(user, committeeConfig);
       const resultPath = path.join(resultsFolder, `${user.userId}-${user.fullName}-${user.committee}.pdf`);
       fs.writeFileSync(resultPath, await userDocument.save());
       successfulUsers.push(new ProcessingResult(user, true, null));
@@ -324,5 +349,5 @@ const processAll = async (processingMode: ProcessingMode, configPath: string) =>
   console.log(`Results saved to ${resultsFolder}`);
 }
 
-processAll(ProcessingMode.AllUsers, "./assets/config.json");
-processAll(ProcessingMode.AllUsers, "./assets/personnelConfig.json");
+processAll(ProcessingMode.AllUsers, "./assets/certificateConfig.json");
+// processAll(ProcessingMode.AllUsers, "./assets/personnelConfig.json");
